bd052e4327ff1252f65e18bb2b3db74b29ac18af
camdenbrown <camdenebrown@gmail.com>
2015-06-15 21:20:52 -0500

    Intermediate Quiz 2

---
+++ quizes/intermediatequiz2/ex1.rb
@@ -0,0 +1,12 @@
+munsters = { 
+  "Herman" => { "age" => 32, "gender" => "male" }, 
+  "Lily" => { "age" => 30, "gender" => "female" }, 
+  "Grandpa" => { "age" => 402, "gender" => "male" }, 
+  "Eddie" => { "age" => 10, "gender" => "male" } 
+}
+
+total_male_age = 0
+
+munsters.each do |name, gender|
+	total_male_age += details["age"] if details["gender"] == "male"
+end
