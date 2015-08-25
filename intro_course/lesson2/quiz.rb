4a48dd6daca98bd02cf52a3f5d18b23a300406d1
camdenbrown <camden.brown@metova.com>
2015-08-25 13:27:31 -0500

    Quiz questions

---
+++ intro_course/lesson2/quiz.rb
@@ -0,0 +1,16 @@
+# 1. local var, instance var, object, getter, setter
+# 2. include
+# 3. Class vars are only available throughout that class and all of it's dependents. Instance vars are available to that class.
+# 4. Sets a getter and setter for an attribute
+# 5. Class method
+# 6. Subclassing is like Dog < Animal. It inherits from Animal. However, modules allow to include blocks of code that might not be common to all Animals
+# 7. class User
+  #   attr_accessor :name
+  #
+  #   def initialize(name)
+  #     @name = name
+  #   end
+  #
+  #  end
+# 8. Yes
+# 9. I first check my terminal to see what the Ruby stack is saying. Then, I backtrack to that code and try to pinpoint where the error might be occuring.
