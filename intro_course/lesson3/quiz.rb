e58f44b6d142fd0582d1c2e0b7d46c4e1cdf0b72
camdenbrown <camden.brown@metova.com>
2015-09-01 17:18:03 -0500

    Quiz in lesson 3

---
+++ intro_course/lesson3/quiz.rb
@@ -0,0 +1,20 @@
+# 1.) Hypertext Markup Language, Cascading Style Sheet, Programming language for web developement
+# 2.) GET and POST
+# 3.) Status codes & Payload
+# 4.) post '/username' do
+#       session[:username] = "Bob"
+#     end
+#
+#     get '/username' do
+#       session[:username]
+#     end
+#
+# 5.) Because we have to design our webpages around the idea that each new page doesn't remember anything unless told
+# 6.) They are the best at handling/displaying the requests. FTP,SMTP,POP3
+# 7.) Model/View/Controller, it's the concept behind Rails.
+#############################################################################
+# 1.) Processed as routes
+# 2.) Render displays a page and redirect sends the user to a route
+# 3.) <%=%>
+# 4.) When the page is rendered
+# 5.) They are available in your actions
